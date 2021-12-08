3767ee3da0cdad719f341d9a7e4d65125d661a98
Matt Rundle <mrundle@amazon.com>
2021-12-08 03:41:00 +0000

    2021: day 7 part 1

---
+++ 2021/7-1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# https://adventofcode.com/2021/day/7
+
+from common import read_input
+
+INPUT_FILE = '7.txt'
+
+def parse_input(input_file):
+    lines = read_input(input_file)
+    assert len(lines) == 1
+    return [int(x) for x in lines[0].split(',')]
+
+def get_median(array):
+    middle_index = (len(array) // 2) - 1
+    return sorted(array)[middle_index]
+
+def main():
+    positions = parse_input(INPUT_FILE)
+    median = get_median(positions)
+    distance = 0
+    for pos in positions:
+        distance += abs(pos - median)
+    print(distance)
+
+if __name__ == '__main__':
+    main()
